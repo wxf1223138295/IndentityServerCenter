2ef464f09588b1e25e8d3324a90f41feafc20776
ShawnWong <wangxf3@rich-healthcare.com>
2019-05-24 09:27:10 +0800

    初始化

---
+++ IdentityCenter/Data/ApplicationUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityCenter.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string CardNumber { get; set; }
+        [Required]
+        public string SecurityNumber { get; set; }
+        [Required]
+        [RegularExpression(@"(0[1-9]|1[0-2])\/[0-9]{2}", ErrorMessage = "Expiration should match a valid MM/YY value")]
+        public string Expiration { get; set; }
+        [Required]
+        public string CardHolderName { get; set; }
+        public int CardType { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string ZipCode { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
